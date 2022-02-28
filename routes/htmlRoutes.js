4bd7da4d78b9556fdb2f911b8c417401ff809f0d
reinholz36 <dreinholz28@gmail.com>
2022-02-28 15:17:51 -0500

    fixed dropdown button issue on journal entry page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -63,7 +63,7 @@ module.exports = (db) => {
         res.render('example', {
           userInfo: req.session.passport.user,
           isloggedin: req.isAuthenticated(),
-          msg: 'Welcome!',
+          msg: 'Welcome! Enter your dreams below',
           examples: dbExamples
         });
       });
